a1f2546e53629fc2e4a198f5f3a40a32054acf9c
Lucute2001 <lch.ryu2001@gmail.com>
2022-10-27 06:00:46 +0700

    graphical representations

--- 2_DS_canh_DS_ke.cpp
+++ 2_DS_canh_DS_ke.cpp
@@ -1,17 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 int n, m;
-int a[1001][1001];
+int a[1001][1001]; //Ma trận kề
+vector<int>adj[1001]; //Ds kề
+// adj[i]: lưu danh sách kề của đỉnh i
+
 int main(){
     system("cls");
     cin >> n >> m;
     for (int i = 0; i < m; i++){
         int x, y; cin >> x >> y;
-        a[x][y] = a[y][x] = 1;
+        adj[x].push_back(y);
+        adj[y].push_back(x);
     }
     for (int i = 1; i <= n; i++){
-        for (int j = 1; j <= n; j++){
-            cout << a[i][j] << ' ';
+        cout << i << " : ";
+        for (int x : adj[i]){
+            cout << x << " ";
         }
         cout << '\n';
     }
